e6daa16d79f072ff899c937a81421ad622bc9f9f
KendrewGW <kendrewgiantoriwidjaja@gmail.com>
2025-01-14 11:49:06 +0700

    add api

---
+++ api/resources/lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    'Thank You for Your Interest' => 'Thank You for Your Interest',
+    'Our team will be contact you within 5 working days' => 'Our team will be contact you within 5 working days',
+    'BACK TO HOME PAGE' => 'BACK TO HOME PAGE',
+]; 
+
+?>
---
+++ api/resources/lang/id/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    'Thank You for Your Interest' => 'Terima Kasih atas Ketertarikannya',
+    'Our team will be contact you within 5 working days' => 'Tim Kami akan menghubungi anda dalam 5 hari kerja',
+    'BACK TO HOME PAGE' => 'Kembali Ke Beranda',
+]; 
+
+?>
