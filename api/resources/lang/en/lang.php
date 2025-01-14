a079b32617d31f8d73db2c817bdae4b7e1ebc382
KendrewGW <kendrewgiantoriwidjaja@gmail.com>
2025-01-14 12:39:33 +0700

    fixing deployment

--- api/resources/lang/en/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return[
-    'Thank You for Your Interest' => 'Thank You for Your Interest',
-    'Our team will be contact you within 5 working days' => 'Our team will be contact you within 5 working days',
-    'BACK TO HOME PAGE' => 'BACK TO HOME PAGE',
-]; 
-
-?>
--- api/resources/lang/id/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return[
-    'Thank You for Your Interest' => 'Terima Kasih atas Ketertarikannya',
-    'Our team will be contact you within 5 working days' => 'Tim Kami akan menghubungi anda dalam 5 hari kerja',
-    'BACK TO HOME PAGE' => 'Kembali Ke Beranda',
-]; 
-
-?>
