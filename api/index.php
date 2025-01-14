9112b67b6f37ffde3818a55dd8c4cc45cda62d9c
KendrewGW <kendrewgiantoriwidjaja@gmail.com>
2025-01-14 13:27:24 +0700

    fixing api

--- api/index.php
+++ api/index.php
@@ -6,11 +6,11 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+// $response = $kernel->handle(
+//     $request = Illuminate\Http\Request::capture()
+// );
 
-$response->send();
+// $response->send();
 
-$kernel->terminate($request, $response);
+// $kernel->terminate($request, $response);
 
