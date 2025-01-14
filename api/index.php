27c55e6005a0b939293d2cfedfbfda83cb7632fb
KendrewGW <kendrewgiantoriwidjaja@gmail.com>
2025-01-14 12:59:34 +0700

    adding kerne;

--- api/index.php
+++ api/index.php
@@ -4,3 +4,13 @@ require __DIR__  . '/../public/index.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
+
